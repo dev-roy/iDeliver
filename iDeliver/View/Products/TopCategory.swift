2012601445b796bd10dcef4f3b12b55a97331e24
Hugo Flores <hfp.ing2@gmail.com>
2020-04-03 22:41:48 -0400

    Category Collection cell component

--- iDeliver/View/Products/TopCategory.swift
+++ iDeliver/View/Products/TopCategory.swift
@@ -8,14 +8,76 @@
 
 import UIKit
 
-class TopCategory: UIView {
+class TopCategory: UICollectionViewCell {
+    
+    static var identifier: String = "TopCategory"
+    
+    static var preferredSize: CGSize = CGSize(width: 110, height: 130)
+    
+    static var imageSize: CGFloat = 90
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var category: Category? {
+        didSet {
+            categoryName.text = category?.name
+            downloadCategoryImage()
+        }
     }
-    */
+    
+    let categoryImage: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFill
+        iv.backgroundColor = .green
+        iv.clipsToBounds = true
+        iv.layer.cornerRadius = imageSize / 2
+        NSLayoutConstraint.activate([
+            iv.widthAnchor.constraint(equalToConstant: imageSize),
+            iv.heightAnchor.constraint(equalToConstant: imageSize),
+        ])
+        return iv
+    }()
+    
+    let categoryName: UILabel = {
+        let lb = UILabel(frame: .zero)
+        lb.font = UIFont(name: "Arial", size: 12)
+        lb.translatesAutoresizingMaskIntoConstraints = false
+        lb.text = "Loading..."
+        lb.textAlignment = .center
+        lb.numberOfLines = 2
+        return lb
+    }()
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpMainContainer()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpMainContainer() {
+        let sv = UIStackView(arrangedSubviews: [categoryImage, categoryName])
+        sv.axis = .vertical
+        sv.alignment = .center
+        sv.spacing = 4
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(sv)
+        NSLayoutConstraint.activate([
+            sv.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            sv.topAnchor.constraint(equalTo: contentView.topAnchor),
+            sv.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            categoryName.leadingAnchor.constraint(equalTo: leadingAnchor),
+            categoryName.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+    }
+    
+    func downloadCategoryImage() {
+        ProductsAPI.getCategoryImage(keywords: category!.name, onDone: setDownloadedImage)
+    }
+    
+    func setDownloadedImage(image imgData: Data?) {
+        guard let image = imgData else { return }
+        categoryImage.image = UIImage(data: image)
+    }
 }
