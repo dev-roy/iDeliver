bc17fe04479f2018363995fe19bb6490ce264c16
Hugo Flores <hfp.ing2@gmail.com>
2020-04-21 18:40:50 -0400

    Notifications keys ennum

---
+++ iDeliver/Model/NotificationEventsKeys.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationEventsKeys.swift
+//  iDeliver
+//
+//  Created by Hugo Flores Perez on 4/21/20.
+//  Copyright © 2020 Field Employee. All rights reserved.
+//
+
+import Foundation
+
+enum NotificationEventsKeys: String {
+    case cartUpdated = "cartUpdated"
+}
