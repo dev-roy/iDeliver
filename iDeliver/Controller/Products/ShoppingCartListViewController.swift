43e9f7006d2adeae40f6d678036a80b9676ba936
Hugo Flores <hfp.ing2@gmail.com>
2020-04-21 20:39:28 -0400

    Cart items list view implemented

--- iDeliver/Controller/Products/ShoppingCartListViewController.swift
+++ iDeliver/Controller/Products/ShoppingCartListViewController.swift
@@ -9,24 +9,69 @@
 import UIKit
 
 class ShoppingCartListViewController: UIViewController {
-    
     static let storyBoardIdentifier: String = "ShoppingCart"
-    
     private var data = [Product]()
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "My Shopping Cart"
+        setUpNavigationBar()
+        setUpTable()
         getCartList()
     }
     
+    func setUpTable() {
+        tableView.dataSource = self
+        tableView.register(ShoppingCartTableCell.self, forCellReuseIdentifier: ShoppingCartTableCell.identifier)
+        view.addSubview(tableView)
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    
+    func setUpNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Checkout", style: .plain, target: self, action: #selector(onCheckout))
+    }
+    
     func getCartList() {
-        ProductsAPI.getShoppingCartItems { products in
+        ProductsAPI.getShoppingCartItems { [unowned self] products in
             self.data = products
-            print("Shopping cart items set")
+            self.tableView.reloadData()
+            print("Data fetched: \(products.count)")
         }
     }
 
+    @objc
+    func onCheckout() {
+        print("To Checkout")
+    }
 
+}
+
+extension ShoppingCartListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ShoppingCartTableCell.identifier, for: indexPath) as! ShoppingCartTableCell
+        cell.delegate = self
+        cell.baseCell?.product = data[indexPath.row]
+        return cell
+    }
+}
+
+extension ShoppingCartListViewController: ShoppingCartCellDelegate {
+    func removeItemFromCart(indexPath: IndexPath) {
+        data.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+    }
 }
