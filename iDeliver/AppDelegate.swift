2db840aa4bda5a8001ae8b92a0cacdd9d987bff9
Hugo Flores <hfp.ing2@gmail.com>
2020-04-23 20:02:58 -0400

    Core Data stack code

--- iDeliver/AppDelegate.swift
+++ iDeliver/AppDelegate.swift
@@ -8,12 +8,35 @@
 
 import UIKit
 import Firebase
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    // MARK: - Core Data Stack
+    lazy var persistentContainer: NSPersistentContainer = {
+      let container = NSPersistentContainer(name: "DataModel")
+      container.loadPersistentStores(completionHandler: { storeDescription, error in
+          if let error = error as NSError? {
+            fatalError("Unresolved error \(error), \(error.userInfo)")
+          }
+      })
+      return container
+    }()
+    
+    func saveContext() {
+      let context = persistentContainer.viewContext
+      if context.hasChanges {
+        do {
+          try context.save()
+        } catch {
+          let error = error as NSError
+          fatalError("Unresolved error \(error), \(error.userInfo)")
+        }
+      }
+    }
 
-
+    // MARK: -
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
