1355c6ccbc003182fa714ef1fe3ed99135ab572e
Hugo Flores <hfp.ing2@gmail.com>
2020-04-24 13:06:18 -0400

    Save context to persistance on resign active

--- iDeliver/SceneDelegate.swift
+++ iDeliver/SceneDelegate.swift
@@ -12,6 +12,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    // MARK: Core Data context
+    private weak var appDelegate = UIApplication.shared.delegate as? AppDelegate
+    private let context = (UIApplication.shared.delegate as? AppDelegate)!.persistentContainer.viewContext
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -35,6 +38,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        appDelegate?.saveContext()
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
